7e0170643ed08d347ceec1be0161cf57aea051c7
Shankey33 <aadidwivedigwl@gmail.com>
2025-08-07 18:12:38 +0530

    Added food category pannel

---
+++ src/assets/food-images/assets.js
@@ -0,0 +1,40 @@
+import butter_naan from './butter-naan.jpg';
+import butter_paneer from './butter-paneer.jpg';
+import dal_fry from './dal-fry.jpg';
+import dal_makhani from './dal-makhani.jpg';
+import dal_tadka from './dal-tadka.jpg';
+import dum_aloo from './dum-aloo.jpg';
+import gulab_jamun from './gulab-jamun.jpg';
+import ice_cream from './ice-cream.jpg';
+import kadai_paneer from './kadai-paneer.jpg';
+import matar_paneer from './matar-paneer.jpg';
+import mix_veg from './mix-veg.jpg';
+import mushroom from './mushroom.jpg';
+import shahi_paneer from './shahi-paneer.jpg';
+import simple_naan from './simple-naan.jpg';
+import tandoori_roti from './tandoori-roti.jpg';
+
+export const food_images_category = [
+    { id: 1, category: "Dish", image: butter_paneer},
+    { id: 2, category: "Dal", image: dal_fry },
+    { id: 3, category: "Breads", image: butter_naan},
+    { id: 4, category: "Sweet", image: gulab_jamun },
+];
+
+export const food_images = [
+    { id: 1, name: 'butter-naan', image: butter_naan, category: "Dish"  },
+    { id: 2, name: 'butter-paneer', image: butter_paneer, category: "Dish" },
+    { id: 3, name: 'dal-fry', image: dal_fry, category: "Dal" },
+    { id: 4, name: 'dal-makhani', image: dal_makhani, category: "Dal" },
+    { id: 5, name: 'dal-tadka', image: dal_tadka, category: "Dal" },
+    { id: 6, name: 'dum-aloo', image: dum_aloo, category: "Dish" },
+    { id: 7, name: 'gulab-jamun', image: gulab_jamun, category: "Sweet" },
+    { id: 8, name: 'ice-cream', image: ice_cream, category: "Sweet" },
+    { id: 9, name: 'kadai-paneer', image: kadai_paneer, category: "Dish" },
+    { id: 10, name: 'matar-paneer', image: matar_paneer, category: "Dish" },
+    { id: 11, name: 'mix-veg', image: mix_veg, category: "Dish" },
+    { id: 12, name: 'mushroom', image: mushroom, category: "Dish" },
+    { id: 13, name: 'shahi-paneer', image: shahi_paneer, category: "Dish" },
+    { id: 14, name: 'simple-naan', image: simple_naan, category: "Breads" },
+    { id: 15, name: 'tandoori-roti', image: tandoori_roti, category: "Breads" }
+];
